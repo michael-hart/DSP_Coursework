eb4e614287f5a2f81a7de4cf70096e762955b6de
Michael Hart <michael.hart1994@gmail.com>
2015-11-08 14:51:16 +0000

    Rename and modified code function.
    Renamed to 2.3i instead of 2.3 to allow for different question part
    numbers. Also changed pulse train to a single pulse, delayed by l.

--- exercise2_3i.m
+++ exercise2_3i.m
@@ -4,24 +4,28 @@
 % I think that means that there will be 49 samples
 
 figure(1);
-legend_vals = [];
+legend_vals = cell(8,1);
 for l = 0:7
-    points = cat(1, zeros(l,1), ones(8,1))';
-    X = fft(points);
+    points = zeros(8, 1);
+    points(l+1) = 1;
+    X = fft(points');
+    
+    % Add all data to plots
     subplot(2,1,1);hold on;
     plot(0:size(X,2)-1, abs(X));
     temp = strcat('Delayed by ', int2str(l));
-    legend_vals(l+1) = strcat('Delayed by ', int2str(l));
+    legend_vals{l+1} = ['Delayed by ' int2str(l)];
     subplot(2,1,2);hold on;
     plot(0:size(X,2)-1, angle(X));
 end
 
+% Add titles, labels and legends
 subplot(2,1,1);
 mag_legends = strcat('Delayed  by ', int2str(0:7));
 legend(legend_vals);
 xlabel('Sample Number');
 ylabel('Magnitude');
-title('DFT of pulse train with varying delays');
+title('DFT of Dirac pulse with varying delays');
 subplot(2,1,2);
 legend(legend_vals);
 xlabel('Sample Number');
